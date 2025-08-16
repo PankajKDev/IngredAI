683dfd644e81bf327dea8e4bce7d1fa17f30d224
PankajKDev <pankajk.dev0000@gmail.com>
2025-08-16 23:45:00 +0530

    [Frontend]:Finished making Pricing cards and page

--- app/(root)/pricing/page.tsx
+++ app/(root)/pricing/page.tsx
@@ -1,9 +1,15 @@
+import PricingCard from "@/components/shared/pricing/PricingCard";
+
 function page() {
   return (
-    <div className="w-full flex flex-col justify-center">
-      <h1 className="text-center font-bold mt-5   text-6xl">
+    <div className="w-full flex flex-col justify-center items-center">
+      <h1 className="text-center font-bold mt-10 font-sans text-4xl  md:text-6xl">
         Fair Pricing plans
       </h1>
+      <div className="w-[80%] gap-5 mt-5 flex justify-center items-center flex-col md:flex-row">
+        <PricingCard />
+        <PricingCard />
+      </div>
     </div>
   );
 }
